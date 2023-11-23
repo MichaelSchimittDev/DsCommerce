eb558d7ef2da6d067755b29a04dbd80795c50097
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-22 22:55:09 -0300

    created user class and configuration h2

---
+++ src/main/java/com/devstarrk/dscommerce/entities/User.java
@@ -0,0 +1,24 @@
+package com.devstarrk.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+}
